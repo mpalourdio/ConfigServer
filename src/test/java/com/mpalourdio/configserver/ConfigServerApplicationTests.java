0d55f1a7dc7e7e0bb8f93bce716077e0bf160c5f
Michel Palourdio <mpalourdio@gmail.com>
2019-03-16 08:26:43 +0100

    Fix member-access

--- src/test/java/com/mpalourdio/configserver/ConfigServerApplicationTests.java
+++ src/test/java/com/mpalourdio/configserver/ConfigServerApplicationTests.java
@@ -13,9 +13,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ConfigServerApplicationTests {
+class ConfigServerApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 }
