45a23ad22e4ce1c858978356d7e99c2e8543e811
Michel Palourdio <mpalourdio@gmail.com>
2022-12-24 01:00:10 +0100

    Disable csrf for /encrypt and /decrypt, and leave basic auth ON for all endpoints

---
+++ src/main/java/com/mpalourdio/configserver/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+/*
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.mpalourdio.configserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        //we disable csrf for command line and leave basic auth enabled on all endpoints
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/decrypt", "/encrypt"))
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic();
+
+
+        return http.build();
+    }
+}
+
